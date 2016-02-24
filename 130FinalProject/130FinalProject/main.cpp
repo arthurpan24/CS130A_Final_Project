b19d2adf37e8ccae601d836c50614bd1b0c7442e
John B. Lanier <johnblanier@gmail.com>
2016-02-23 16:50:30 -0800

    New main and run schemes

--- 130FinalProject/130FinalProject/main.cpp
+++ 130FinalProject/130FinalProject/main.cpp
@@ -1,15 +1,52 @@
-//
-//  main.cpp
-//  130FinalProject
-//
-//  Created by JB on 2/23/16.
-//  Copyright © 2016 JB Lanier. All rights reserved.
-//
+/*
+ * Main.cpp
+ *
+ *  Created on: Feb 11, 2016
+ *      Author: omid
+ */
 
+#include <string>
+#include <sstream>
+#include <fstream>
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+vector<string> split(string str, char delimiter)
+{
+    vector<string> internal;
+    stringstream ss(str);       // turn the string into a stream.
+    string tok;
+    
+    while(getline(ss, tok, delimiter))
+    {
+        internal.push_back(tok);
+    }
+    
+    return internal;
+}
+
+int main(int argc, char** argv)
+{
+    string path = "/home/omid55/PythonProjects/RecordGenerator/Generated1.txt";
+    if(argc > 1)
+    {
+        path = argv[1];
+    }
+    
+    ifstream f;
+    f.open(path.c_str(), ios::in);
+    if(!f) cerr << "File not found." << endl;
+    else
+    {
+        while(!f.eof())
+        {
+            string line;
+            std::getline(f, line);
+            vector<string> words = split(line, ',');
+            cout << words.size() << endl;
+            // ... TO DO
+        }
+    }
     return 0;
 }
